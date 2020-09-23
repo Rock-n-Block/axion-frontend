c8f75e4876551d6c87260d54e8adc0f5b83a9ef4
Amary <laumiqv@gmail.com>
2020-09-23 12:02:42 +0300

    set cookies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import {AfterContentInit, Component, NgZone, ViewChild, OnInit} from '@angular/core';
-import {TransactionSuccessModalComponent} from './components/transactionSuccessModal/transaction-success-modal.component';
-import {MetamaskErrorComponent} from './components/metamaskError/metamask-error.component';
-import { ContractService} from './services/contract';
-import {MatDialog} from '@angular/material/dialog';
-import {NavigationStart, Router} from '@angular/router';
+import { Component, NgZone, ViewChild, OnInit } from "@angular/core";
+import { TransactionSuccessModalComponent } from "./components/transactionSuccessModal/transaction-success-modal.component";
+import { MetamaskErrorComponent } from "./components/metamaskError/metamask-error.component";
+import { ContractService } from "./services/contract";
+import { MatDialog } from "@angular/material/dialog";
+import { ActivationStart, NavigationStart, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
   public isNavbarOpen;
@@ -18,29 +18,32 @@ export class AppComponent implements OnInit {
   public leftDaysInfo;
   public tableInfo;
   public runLineCountArray = new Array(1);
-  @ViewChild('runString', {static: false}) runString;
+  @ViewChild("runString", { static: false }) runString;
   constructor(
     private contractService: ContractService,
     private ngZone: NgZone,
     public dialog: MatDialog,
-    route: Router,
+    route: Router
   ) {
+    this.accountSubscribe = this.contractService
+      .accountSubscribe()
+      .subscribe((account) => {
+        if (account) {
+          this.accountSubscribe.unsubscribe();
+          this.subscribeAccount();
+        }
+      });
 
-    this.accountSubscribe = this.contractService.accountSubscribe().subscribe((account) => {
-      if (account) {
-        this.accountSubscribe.unsubscribe();
-        this.subscribeAccount();
-      }
-    });
-
-    this.contractService.transactionsSubscribe().subscribe((transaction: any) => {
-      if (transaction) {
-        this.dialog.open(TransactionSuccessModalComponent, {
-          width: '400px',
-          data: transaction.hash
-        });
-      }
-    });
+    this.contractService
+      .transactionsSubscribe()
+      .subscribe((transaction: any) => {
+        if (transaction) {
+          this.dialog.open(TransactionSuccessModalComponent, {
+            width: "400px",
+            data: transaction.hash,
+          });
+        }
+      });
     this.contractService.getAccount(true);
 
     this.contractService.getEndDateTime().then((result) => {
@@ -56,6 +59,14 @@ export class AppComponent implements OnInit {
     this.isHeaderActive = false;
 
     route.events.subscribe((event) => {
+      if (event instanceof ActivationStart) {
+        if (event.snapshot.queryParams.ref) {
+          document.cookie = `ref=${event.snapshot.queryParams.ref}; ${
+            new Date().getTime() + 5 * 24 * 60 * 60 * 1000
+          };''`;
+        }
+      }
+
       if (event instanceof NavigationStart) {
         window.scrollTo(0, 0);
       }
@@ -70,32 +81,42 @@ export class AppComponent implements OnInit {
     if (this.account) {
       return;
     }
-    this.accountSubscribe = this.contractService.accountSubscribe().subscribe((account: any) => {
-      this.ngZone.run(() => {
-        if (account && (!this.account || this.account.address !== account.address)) {
-          this.contractService.loadAccountInfo();
-        }
-        this.account = account;
+    this.accountSubscribe = this.contractService
+      .accountSubscribe()
+      .subscribe((account: any) => {
+        this.ngZone.run(() => {
+          if (
+            account &&
+            (!this.account || this.account.address !== account.address)
+          ) {
+            this.contractService.loadAccountInfo();
+          }
+          this.account = account;
+        });
       });
-    });
     this.contractService.getAccount().catch((err) => {
       this.dialog.open(MetamaskErrorComponent, {
-        width: '400px',
-        data: err
+        width: "400px",
+        data: err,
       });
     });
   }
 
   iniRunString() {
     const runStringElement = this.runString.nativeElement;
-    const runStringItem = runStringElement.getElementsByClassName('repeat-content')[0];
-    this.runLineCountArray.length = Math.ceil(runStringElement.offsetWidth / runStringItem.offsetWidth) * 2;
+    const runStringItem = runStringElement.getElementsByClassName(
+      "repeat-content"
+    )[0];
+    this.runLineCountArray.length =
+      Math.ceil(runStringElement.offsetWidth / runStringItem.offsetWidth) * 2;
 
     setInterval(() => {
-      const allElements = runStringElement.getElementsByClassName('repeat-content');
-      const marginLeft = allElements[0].style.marginLeft || '0px';
-      const newMarginLeft = marginLeft.replace('px', '') - 1;
-      allElements[0].style.marginLeft = newMarginLeft + 'px';
+      const allElements = runStringElement.getElementsByClassName(
+        "repeat-content"
+      );
+      const marginLeft = allElements[0].style.marginLeft || "0px";
+      const newMarginLeft = marginLeft.replace("px", "") - 1;
+      allElements[0].style.marginLeft = newMarginLeft + "px";
       if (-newMarginLeft > allElements[0].offsetWidth) {
         allElements[0].style.marginLeft = 0;
         runStringElement.appendChild(allElements[0]);
@@ -110,7 +131,6 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    window.addEventListener('scroll', this.onScrollWindow.bind(this));
+    window.addEventListener("scroll", this.onScrollWindow.bind(this));
   }
-
 }
