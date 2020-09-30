fbbfb56fcf80e99355aaf459a8b391eccb565dff
Amary <laumiqv@gmail.com>
2020-09-30 22:18:54 +0300

    hex2t logo

--- src/app/components/transactionSuccessModal/transaction-success-modal.component.ts
+++ src/app/components/transactionSuccessModal/transaction-success-modal.component.ts
@@ -1,19 +1,19 @@
-import {Component, Inject} from '@angular/core';
-import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Component, Inject } from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 
 @Component({
-  selector: 'app-transaction-success-modal',
-  templateUrl: './transaction-success-modal.component.html'
+  selector: "app-transaction-success-modal",
+  templateUrl: "./transaction-success-modal.component.html",
 })
 export class TransactionSuccessModalComponent {
-    public ethLink: string;
-    constructor(
-        public dialogRef: MatDialogRef<TransactionSuccessModalComponent>,
-        @Inject(MAT_DIALOG_DATA) public data: any
-        ) {
-            this.ethLink = `https://rinkeby.etherscan.io/tx/${data}`;
-        }
-    public closeModal() {
-        this.dialogRef.close();
-    }
+  public ethLink: string;
+  constructor(
+    public dialogRef: MatDialogRef<TransactionSuccessModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.ethLink = `https://ropsten.etherscan.io/tx/${data}`;
+  }
+  public closeModal() {
+    this.dialogRef.close();
+  }
 }
