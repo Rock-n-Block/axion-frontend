266ec07fedb66d17cb016e497d0ee9fc3be10407
Amary <laumiqv@gmail.com>
2020-10-13 17:45:45 +0300

    testnet

--- src/app/params.ts
+++ src/app/params.ts
@@ -1,49 +1,19 @@
 // ropsten
 
-export const chackerDays = 3600000;
-export const chackerAuctionPool = 15000;
-export const chackerStakingInfo = 3600000;
-export const chackerBPD = 3600000;
-
-export const settingsData = {
-  settings: {
-    production: false,
-    network: "ropsten",
-    tonkenUrl: "https://rinkeby.etherscan.io/token/",
-    net: 3,
-    time: {
-      seconds: 86400,
-      display: "days",
-    },
-  },
-  minutes: {
-    name: "Minutes",
-    shortName: "Min",
-    lowerName: "minutes",
-  },
-  days: {
-    name: "Days",
-    shortName: "Days",
-    lowerName: "days",
-  },
-};
-
-// rinkeby
-
-// export const chackerDays = 5000;
-// export const chackerAuctionPool = 5000;
+// export const chackerDays = 3600000;
+// export const chackerAuctionPool = 15000;
 // export const chackerStakingInfo = 3600000;
 // export const chackerBPD = 3600000;
-//
+
 // export const settingsData = {
 //   settings: {
 //     production: false,
-//     network: "rinkeby",
+//     network: "ropsten",
 //     tonkenUrl: "https://rinkeby.etherscan.io/token/",
-//     net: 4,
+//     net: 3,
 //     time: {
-//       seconds: 900,
-//       display: "minutes",
+//       seconds: 86400,
+//       display: "days",
 //     },
 //   },
 //   minutes: {
@@ -57,3 +27,33 @@ export const settingsData = {
 //     lowerName: "days",
 //   },
 // };
+
+// rinkeby
+
+export const chackerDays = 5000;
+export const chackerAuctionPool = 5000;
+export const chackerStakingInfo = 3600000;
+export const chackerBPD = 3600000;
+
+export const settingsData = {
+  settings: {
+    production: false,
+    network: "rinkeby",
+    tonkenUrl: "https://rinkeby.etherscan.io/token/",
+    net: 4,
+    time: {
+      seconds: 900,
+      display: "minutes",
+    },
+  },
+  minutes: {
+    name: "Minutes",
+    shortName: "Min",
+    lowerName: "minutes",
+  },
+  days: {
+    name: "Days",
+    shortName: "Days",
+    lowerName: "days",
+  },
+};
